6cbaa39e345b710e72776de5509623c394ac9b50
Вадим Бард <yvadis@mail.ru>
2025-05-29 10:28:18 +0700

    Creted a service struct

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,1 +1,27 @@
 package service
+
+type QuoteService interface {
+	CreateQuote(quote Quote) error
+	GetAllQuotes() ([]Quote, error)
+	GetRandomQuote() (Quote, error)
+}
+
+type quoteService struct {
+	repo QuoteRepository
+}
+
+func NewQuoteService(r QuoteRepository) QuoteService {
+	return &quoteService{repo: r}
+}
+
+func (s *quoteService) CreateQuote(quote Quote) error {
+	return s.repo.CreateQuote(quote)
+}
+
+func (s *quoteService) GetAllQuotes() ([]Quote, error) {
+	return s.repo.GetAllQuotes()
+}
+
+func (s *quoteService) GetRandomQuote() (Quote, error) {
+	return s.repo.GetRandomQuote()
+}
