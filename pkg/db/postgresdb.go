5c2416e176167c88316ceaf94da3f9270781e6fc
Вадим Бард <yvadis@mail.ru>
2025-05-29 10:13:24 +0700

    Created a DB struct

---
+++ pkg/db/postgresdb.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"OnlyGo/pkg/service"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost port=5432 user=postgres password=yourpassword dbname=postgres sslmode=disable"
+	var err error
+
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("FATAL: Ошибка подключения к БД: %v", err)
+	}
+	if err := db.AutoMigrate(&service.Quote{}); err != nil {
+		log.Fatalf("FATAL: Ошибка миграции: %v", err)
+	}
+
+	return db, nil
+}
+
