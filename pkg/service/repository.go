673a362f93031f9e3ba77ce03563a4b406ba0a1f
Вадим Бард <yvadis@mail.ru>
2025-05-29 10:13:51 +0700

    Create repository

--- pkg/service/repository.go
+++ pkg/service/repository.go
@@ -1,1 +1,36 @@
 package service
+
+import (
+	"gorm.io/gorm"
+)
+
+type QuoteRepository interface {
+	CreateQuote(quote Quote) error
+	GetAllQuotes() ([]Quote, error)
+	GetQuoteByID(quote_id string) (Quote, error)
+}
+
+type quoteRepository struct {
+	db *gorm.DB
+}
+
+func NewQuoteRepository(db *gorm.DB) QuoteRepository {
+	return &quoteRepository{db: db}
+}
+
+
+func (r *quoteRepository) CreateQuote(quote Quote) error {
+	return r.db.Create(&quote).Error
+}
+
+func (r *quoteRepository) GetAllQuotes() ([]Quote, error) {
+	var quotes []Quote
+	err := r.db.Find(&quotes).Error
+	return quotes, err
+}
+
+func (r *quoteRepository) GetQuoteByID(quote_id string) (Quote, error) {
+	var quote Quote
+	err := r.db.First(&quote, "id = ?", quote_id).Error
+	return quote, err
+}
