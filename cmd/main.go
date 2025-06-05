ea71a711f98959254cda93a337742f6e938c4ca5
Вадим Бард <yvadis@mail.ru>
2025-06-05 14:27:59 +0700

    Add config

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"OnlyGo/logging"
+	"OnlyGo/pkg/config"
 	"OnlyGo/pkg/db"
 	"OnlyGo/pkg/quote"
+	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"os"
+	"path"
+	"path/filepath"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
+const (
+	SocketFile = "app.sock"
+	Sock = "sock"
+)
+
 func main() {
 	logger := logging.GetLogger()
+	cfg := config.GetConfig()
 
 	logger.Info("Start db")
 	database, err := db.InitDB()
@@ -31,13 +42,31 @@ func main() {
 	r := mux.NewRouter()
 	quoteHandler.Register(r)
 
-	start(r)
+	start(r, cfg)
 }
 
-func start(router *mux.Router) {
-	listener, err := net.Listen("tcp", ":8080")
-	if err != nil {
-		log.Fatal(err)
+func start(router *mux.Router, cfg *config.Config) {
+	logger := logging.GetLogger()
+	
+	var listener net.Listener
+	var listenError error
+
+	if cfg.Listen.Type == Sock {
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("Create socket")
+		socketPath := path.Join(appDir, SocketFile)
+		logger.Debugf("socket path: %s", socketPath)
+
+		listener, listenError = net.Listen("unix", socketPath)	
+	} else {
+		listener, listenError = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+	}
+
+	if listenError != nil {
+		logger.Fatal(listenError)
 	}
 
 	server := &http.Server{
