f993d4405034656e019c4a49efe9405bbf0db60c
Вадим Бард <yvadis@mail.ru>
2025-06-05 14:27:16 +0700

    Add .env in db

--- pkg/db/postgresdb.go
+++ pkg/db/postgresdb.go
@@ -2,8 +2,11 @@ package db
 
 import (
 	"OnlyGo/pkg/quote"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -11,7 +14,21 @@ import (
 var db *gorm.DB
 
 func InitDB() (*gorm.DB, error) {
-	dsn := "host=localhost port=5432 user=postgres password=yourpassword dbname=postgres sslmode=disable"
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("FATAL: Ошибка загрузки .env файла: %v", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("SSL_MODE")
+	
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host, port, user, password, dbname, sslmode,
+	)
 	var err error
 
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
